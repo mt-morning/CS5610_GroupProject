fc323eaf3aa2ebbebb3e6c34cae4e129e936e4c9
Tran Pham <pham.tr@husky.neu.edu>
2020-07-31 19:53:05 -0400

    update product filter: control components in forms

--- ui/src/ProductFilter.jsx
+++ ui/src/ProductFilter.jsx
@@ -5,35 +5,72 @@ import { withRouter } from 'react-router-dom';
 import URLSearchParams from 'url-search-params';
 
 class ProductFilter extends React.Component {
-  constructor() {
+  constructor({ location: { search } }) {
     super();
+	const params = new URLSearchParams(search);
+	this.state = {
+		status: params.get('status') || ``,
+		changed: false,
+	};
+	
     this.onChangeStatus = this.onChangeStatus.bind(this);
+	this.applyFilter = this.applyFilter.bind(this);
+  }
+
+  componentDidUpdate(prevProps) {
+    const { location: { search: prevSearch } } = prevProps;
+    const { location: { search } } = this.props;
+    if (prevSearch !== search) {
+      this.showOriginalFilter();
+    }
   }
 
   onChangeStatus(e) {
-    const quantity = e.target.value;
-    const { history } = this.props;
-    history.push({
-      pathname: '/products',
-      search: quantity ? `?quantity=${quantity}` : '',
-    });
+	this.setState({ status: e.target.value, changed: true });
   }
 
+  showOriginalFilter() {
+    const { location: { search } } = this.props;
+    const params = new URLSearchParams(search);
+    this.setState({
+      status: params.get('status') || “,
+      changed: false,
+    });
+  }
+  applyFilter() {
+    const { status } = this.state;
+    const { history } = this.props;
+    history.push({
+      pathname: '/issues',
+      search: status ? `?status=${status}` : “,
+    });
+  }
+  
   // TODO: replace select with React component for slider or selector
   // TODO: replace hard coded values; incorporate lte or gte...?
   render() {
+	const { status } = this.state;
 	const { location: { search } } = this.props;
     const params = new URLSearchParams(search);
     return (
       <div>
         Filter by quantity:
         {' '}
-        <select value={params.get('status') || '' } 
-		onChange={this.onChangeStatus}>
+		<select value={status} onChange={this.onChangeStatus}>
           <option value="">(All Products)</option>
           <option value="1">Low Stock (1)</option>
           <option value="0">Out of Stock (0)</option>
         </select>
+		{' '}
+        <button type="button" onClick={this.applyFilter}>Apply</button>
+		{' '}
+        <button
+          type="button"
+          onClick={this.showOriginalFilter}
+          disabled={!changed}
+        >
+          Reset
+        </button>
       </div>
     );
   }
