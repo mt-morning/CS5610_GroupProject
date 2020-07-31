903ebbbc7be2f90f90098bf2111c5877c8795d19
Tran Pham <pham.tr@husky.neu.edu>
2020-07-31 19:54:47 -0400

    linted files

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -2,14 +2,9 @@ import 'babel-polyfill';
 import 'whatwg-fetch';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-import { BrowserRouter as Router } from 'react-router-dom'; 
 import Page from './Page.jsx';
-
-// import React, { Component } from 'react';
-// import { BrowserRouter as Router, Route, Link} from 'react-router-dom';
-// import { browserHistory } from 'react-router';
-// import HomePage from './HomePage';
 
 const element = (
   <Router>
@@ -17,7 +12,7 @@ const element = (
   </Router>
 );
 
-ReactDOM.render(element, document.getElementById('content'));
+ReactDOM.render(element, document.getElementById('contents'));
 
 if (module.hot) {
   module.hot.accept();
