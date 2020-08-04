c3da9b516fb36721b1fb391dbd63ba2478ec6ba3
Miranda <mtran3@wellesley.edu>
2020-08-04 12:41:45 -0400

    fix lint spacing

--- src/Page.jsx
+++ src/Page.jsx
@@ -1,55 +1,55 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import {
-    Navbar, Nav, NavItem, NavDropdown,
-    MenuItem, Glyphicon, Tooltip, OverlayTrigger, Grid,
+  Navbar, Nav, NavItem, NavDropdown,
+  MenuItem, Glyphicon, Tooltip, OverlayTrigger, Grid,
 } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 
 import Contents from './Contents.jsx';
 
 function NavBar() {
-    return (
-        <Navbar>
-            <Navbar.Header>
-                <Navbar.Brand>Inventory Tracker</Navbar.Brand>
-            </Navbar.Header>
+  return (
+    <Navbar>
+      <Navbar.Header>
+        <Navbar.Brand>Inventory Tracker</Navbar.Brand>
+      </Navbar.Header>
 
-            <Nav>
-                <LinkContainer exact to="/"><NavItem>Home</NavItem></LinkContainer>
-  
-                <LinkContainer to="/signin"><NavItem>Sign in</NavItem></LinkContainer>
-  
-                <LinkContainer to="/products"><NavItem>Inventory Overview </NavItem></LinkContainer>
+      <Nav>
+        <LinkContainer exact to="/"><NavItem>Home</NavItem></LinkContainer>
 
-                <LinkContainer to="/report"><NavItem>Report</NavItem></LinkContainer>
+        <LinkContainer to="/signin"><NavItem>Sign in</NavItem></LinkContainer>
 
-                <LinkContainer to="/remove"><NavItem>Remove Product</NavItem></LinkContainer>
+        <LinkContainer to="/products"><NavItem>Inventory Overview </NavItem></LinkContainer>
 
-            </Nav>
+        <LinkContainer to="/report"><NavItem>Report</NavItem></LinkContainer>
 
-                <Nav pullRight>
-                    <NavItem>
-                        <OverlayTrigger
-                            placement="left"
-                            delayShow={1000}
-                            overlay={<Tooltip id="/products">Add Product</Tooltip>}
-                                >
-                                <Glyphicon glyph="plus" />
-                                </OverlayTrigger>
-                            </NavItem>
-            </Nav>
-        </Navbar>
+        <LinkContainer to="/remove"><NavItem>Remove Product</NavItem></LinkContainer>
+
+      </Nav>
+
+      <Nav pullRight>
+        <NavItem>
+          <OverlayTrigger
+            placement="left"
+            delayShow={1000}
+            overlay={<Tooltip id="/products">Add Product</Tooltip>}
+          >
+            <Glyphicon glyph="plus" />
+          </OverlayTrigger>
+        </NavItem>
+      </Nav>
+    </Navbar>
   );
 }
 
 export default function Page() {
   return (
     <div>
-          <NavBar />
-          <Grid fluid>
-              <Contents />
-              </Grid>
+      <NavBar />
+      <Grid fluid>
+        <Contents />
+      </Grid>
     </div>
   );
 }
