248f76c5dd7ce88241f5b08c93cdc2008736b8bc
Miranda <mtran3@wellesley.edu>
2020-08-12 08:41:38 -0400

    remove unused components

--- src/SignInNavItem.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function SignInNavItem() {
-  return (
-    <div>
-      <h2>This is a placeholder for the Sign in Functionality</h2>
-    </div>
-  );
-}
